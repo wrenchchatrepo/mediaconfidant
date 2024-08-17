5bb139f04266810c4e32a8bedf792490e3e8b011
dion edge <158282973+wrenchchat@users.noreply.github.com>
2024-08-16 19:01:21 -0500

    Looker (#70)
    * Cleared out all files from prod branch
    
    * add readme.md
    
    * Create hold.txt
    
    * add pulumi import
    
    * pulumi github actions
    
    * Create hold.sql
    
    * Update hold.sql
    
    * Create sqlx
    
    * Rename models/vendors.model.lkml to looker/demo/vendors.model.lkml
    
    * Add files via upload
    
    * Rename views/all_data_temp.view.lkml to views/looker /demo/all_data_temp.view.lkml
    
    * Rename views/.gitkeep to looker/demo/views/.gitkeep
    
    * Rename views/demo_bing.view.lkml to looker/demo/views/demo_bing.view.lkml
    
    * Rename demo_bing.view.lkml to demo_bing.view.lkml
    
    * Rename views/demo_facebook.view.lkml to looker/demo/views /demo_facebook.view.lkml
    
    * Rename models/.gitkeep to looker/demo/views /.gitkeep
    
    * Rename views/demo_ga4.view.lkml to looker/demo/views /demo_ga4.view.lkml
    
    * Rename views/demo_tiktok.view.lkml to looker/demo/views /demo_tiktok.view.lkml
    
    * Rename views/demo_google.view.lkml to looker/demo/views /demo_google.view.lkml
    
    * Delete looker/demo/views directory
    
    * Create ga4.model.lkml
    
    * Create block_google_ads_transfer_new_v2.model.lkmlblock_google_ads_transfer_new_v2.model.lkml
    
    * Add files via upload
    
    * Create hold.lkml
    
    * Add files via upload
    
    * Add files via upload
    
    * Create spectacles.md
    
    * Rename all_data_temp.view.lkml to all_data_temp.view.lkml
    
    * Delete viewslooker/demo/views/all_data_temp.view.lkml
    
    * Create Agentic_System.md
    
    * Add files via upload
    
    * Rename spectacles.md to spectacles/spectacles.md
    
    * Create all_data_temp
    
    * Update README.md
    
    * Update README.md
    
    * Create LICENSE
    
    * Merge pull request #62 from wrenchchatrepo/prod (#63)
    
    * Create all_data_temp.sqlx
    
    * Delete definitions directory
    
    * Update README.md
    
    * Handout
    
    * Add files via upload
    
    * Delete looker/demo/views directory
    
    * Add files via upload
    
    * Create all_data_temp.view.lkml
    
    * Delete looker/demo/views directory
    
    * Create hold.txt
    
    * Delete looker/marketplace_google-ads-transfer-v2/views/hold.lkml
    
    * Delete looker/vendors/hold.txt
    
    * Delete containers directory
    
    * Delete bigquery directory
    
    * Delete spectacles directory
    
    * Delete Agentic_System.md
    
    * Delete Mediaconfidant diagram.pdf
    
    * Delete README.md
    
    * Delete LICENSE
    
    * Delete budget_report.png
    
    * Delete looker/marketplace_google-ads-transfer-v2 directory
    
    * Delete looker/marketplace_ga4 directory
    
    * Create hold.txt
    
    * Create hold.txt

--- bigquery/ml/evaluate.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM ML.EVALUATE(MODEL `doit-new-project.customers.linear_regression_model`);
-
-SELECT * FROM ML.EVALUATE(MODEL `doit-new-project.customers.logistic_regression_model`);
-
-SELECT * FROM ML.EVALUATE(MODEL `doit-new-project.customers.kmeans_model`);
-
-SELECT * FROM ML.EVALUATE(MODEL `doit-new-project.customers.time_series_model`);
-
-SELECT * FROM ML.EVALUATE(MODEL `doit-new-project.customers.random_forest_model`);
